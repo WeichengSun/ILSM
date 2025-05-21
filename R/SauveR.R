22b9b32f11dc5cf8c0b2c576816da1aa897aa009
yclab <yanch1985@gmail.com>
2025-05-21 15:06:35 +0800

    readme Sauve

--- R/SauveR.R
+++ R/SauveR.R
@@ -2,19 +2,19 @@ SauveR<-function(N,m,type=c("row","col")){
    M<-emptyR(m)
    web <- as.matrix(M[[1]])
    if(type=="row"){
-      savue.fastr <- function(web,m,M) {
+      sauve.fastr <- function(web,m,M) {
          web[sample(sample(nrow(web))),]<-web
          m[M[[2]],M[[3]]]<-web
          return(m)
       }
-      return(replicate(N, savue.fastr(web,m,M), simplify = FALSE))
+      return(replicate(N, sauve.fastr(web,m,M), simplify = FALSE))
    }
    else{
-      savue.fastc <- function(web,m,M) {
+      sauve.fastc <- function(web,m,M) {
          web[,sample(ncol(web))]<-web
          m[M[[2]],M[[3]]]<-web
          return(m)
       }
-      return(replicate(N, savue.fastc(web,m,M), simplify = FALSE))
+      return(replicate(N, sauve.fastc(web,m,M), simplify = FALSE))
    }
 }
